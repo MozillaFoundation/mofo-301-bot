918298563ce69d40f2747f4d704d1c130266f1be
Christopher De Cairos <cade@mozillafoundation.org>
2016-02-10 17:32:08 -0500

    Tweak the handling of the secure connection param

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const botNick = process.env.NICK;
 const ircClientOptions = {
   channels: process.env.CHANNEL_LIST.split(' '),
   port: process.env.IRC_PORT,
-  secure: process.env.SECURE_CONNECTION | false,
+  secure: process.env.SECURE_CONNECTION === true,
   autoRejoin: true,
   floodProtection: true,
   floodProtectionDelay: 1000
